aa8865fc27fd8f653d1915d2106209c3e1cae0c8
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2021-03-04 13:02:40 +0700

    Thai commit 3/4/2021 1:02pm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Header from './components/Header';
+import Section from './components/Section';
+import './components/css/Header.css';
+import {DataProvider} from './components/Context';
+import Footer from './components/Footer';
+class App extends React.Component{
+  render() {
+    return (
+      <DataProvider>
+        <div className="app"> 
+          <Router>
+            <Header/>
+            <Section/>
+            <Footer/>
+          </Router>
+        </div>
+      </DataProvider>
+      );
+  }
 }
 
 export default App;
